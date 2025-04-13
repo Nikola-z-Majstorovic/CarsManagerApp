515b0e8dca7ae6604b0120334eb2c26682d83a58
Nikola Majstorovic <nikola.majstorovic@gecko.rs>
2025-04-13 22:00:56 +0200

    db support and unit tests

---
+++ MyBackend/Repositores/CarRepository.cs
@@ -0,0 +1,35 @@
+﻿using MyBackend.Context;
+using MyBackend.Models.Entity;
+using MyBackend.Repositores.Interfaces;
+
+namespace MyBackend.Repositores
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Car> GetAll() => _context.Cars.ToList();
+
+        public void AddCar(Car car)
+        {
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+        }
+
+        public void DeleteCar(int id)
+        {
+            var car = _context.Cars.Find(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+            }
+        }
+    }
+
+}
